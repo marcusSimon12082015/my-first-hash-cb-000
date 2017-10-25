fe9356173b92fcd80c0d2df75bf3e96da5416c6a
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-25 19:20:24 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_hash = {}
+  my_hash["element1"] = 1
+  my_hash["element2"] = 2
+  my_hash["element3"] = 3
 end
 
 
